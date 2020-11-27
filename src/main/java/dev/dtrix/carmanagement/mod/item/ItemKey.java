27d5452551469e0250c55ec5d25f57a18bd5c663
Alexy Mantha <alexy.mantha.1@ulaval.ca>
2020-11-26 19:39:23 -0500

    Add key item

---
+++ src/main/java/dev/dtrix/carmanagement/mod/item/ItemKey.java
@@ -0,0 +1,17 @@
+package dev.dtrix.carmanagement.mod.item;
+
+import dev.dtrix.carmanagement.CarManagementAddon;
+import dev.dtrix.carmanagement.mod.CarManagementMod;
+import net.minecraft.item.Item;
+
+public class ItemKey extends Item {
+
+    public ItemKey() {
+        setCreativeTab(CarManagementMod.TAB);
+        setMaxStackSize(1);
+        setNoRepair();
+        setRegistryName(CarManagementAddon.MODID, "car_key");
+        setUnlocalizedName("item_key");
+    }
+
+}
---
+++ src/main/java/dev/dtrix/carmanagement/mod/item/Items.java
@@ -0,0 +1,40 @@
+package dev.dtrix.carmanagement.mod.item;
+
+import dev.dtrix.carmanagement.CarManagementAddon;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@Mod.EventBusSubscriber
+public class Items {
+
+    public static final Item KEY = new ItemKey();
+
+    @SubscribeEvent
+    public static void onItemRegister(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(KEY);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public static void onModelRegister(ModelRegistryEvent event) {
+        registerModel(KEY);
+    }
+
+    @SideOnly(Side.CLIENT)
+    private static void registerModel(Item item) {
+        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName().toString(), "inventory"));
+    }
+
+    @SideOnly(Side.CLIENT)
+    private static void registerModel(Item item, int metadata, String name) {
+        ModelLoader.setCustomModelResourceLocation(item, metadata, new ModelResourceLocation(CarManagementAddon.MODID + ":" + name, "inventory"));
+    }
+
+}
