92f7bfc88c5b70d8933ded4c9d76b67ee763f462
Alexy Mantha <mrredstone231@gmail.com>
2021-07-04 17:52:55 -0400

    Add retrieve feature

---
+++ src/main/java/dev/dtrix/carmanagement/mod/packets/PacketRetrieveVehicle.java
@@ -0,0 +1,55 @@
+package dev.dtrix.carmanagement.mod.packets;
+
+import com.jme3.math.Vector3f;
+import dev.dtrix.carmanagement.client.gui.GuiGarage;
+import dev.dtrix.carmanagement.garage.GarageManager;
+import dev.dtrix.carmanagement.garage.StoredVehicle;
+import fr.dynamx.common.entities.vehicles.CarEntity;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.voxelindustry.brokkgui.wrapper.impl.BrokkGuiManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PacketRetrieveVehicle implements IMessage {
+
+    public StoredVehicle vehicle;
+
+    public PacketRetrieveVehicle() {}
+
+    public PacketRetrieveVehicle(StoredVehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    @Override
+    public void fromBytes(ByteBuf buf) {
+        vehicle = new StoredVehicle();
+        vehicle.deserializeNBT(ByteBufUtils.readTag(buf));
+    }
+
+    @Override
+    public void toBytes(ByteBuf buf) {
+        ByteBufUtils.writeTag(buf, vehicle.serializeNBT());
+    }
+
+    public static class Handler implements IMessageHandler<PacketRetrieveVehicle, IMessage> {
+
+        @Override
+        public IMessage onMessage(PacketRetrieveVehicle message, MessageContext ctx) {
+            ctx.getServerHandler().player.getServerWorld().addScheduledTask(() -> {
+                if(GarageManager.getStorage().retrieve(ctx.getServerHandler().player, message.vehicle)) {
+                    System.out.println("spawning vehicle...");
+                    CarEntity vehicle = new CarEntity(message.vehicle.getName(), ctx.getServerHandler().player.world, new Vector3f(((float) ctx.getServerHandler().player.posX), ((float) (ctx.getServerHandler().player.posY + 10)), ((float) ctx.getServerHandler().player.posZ)), 0, message.vehicle.getMetadata());
+                    ctx.getServerHandler().player.world.spawnEntity(vehicle);
+                }
+            });
+            return null;
+        }
+
+    }
+}
