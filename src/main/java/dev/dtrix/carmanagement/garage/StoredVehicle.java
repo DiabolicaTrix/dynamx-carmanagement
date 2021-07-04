29bd9681a536d100ad4abf7c1e214e6a1252b33e
Alexy Mantha <mrredstone231@gmail.com>
2021-07-04 14:34:21 -0400

    Add stored vehicle implementation

---
+++ src/main/java/dev/dtrix/carmanagement/garage/StoredVehicle.java
@@ -0,0 +1,62 @@
+package dev.dtrix.carmanagement.garage;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.util.INBTSerializable;
+
+public class StoredVehicle implements INBTSerializable<NBTTagCompound> {
+
+    private int id;
+    private String name;
+    private int metadata;
+
+    public StoredVehicle() {}
+
+    public StoredVehicle(int id, String name, int metadata) {
+        this.id = id;
+        this.name = name;
+        this.metadata = metadata;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(int metadata) {
+        this.metadata = metadata;
+    }
+
+    @Override
+    public NBTTagCompound serializeNBT() {
+        NBTTagCompound nbt = new NBTTagCompound();
+        nbt.setInteger("id", this.id);
+        nbt.setInteger("metadata", this.metadata);
+        nbt.setString("name", this.name);
+        return nbt;
+    }
+
+    @Override
+    public void deserializeNBT(NBTTagCompound nbt) {
+        if(nbt.hasKey("id")) {
+            this.id = nbt.getInteger("id");
+            this.metadata = nbt.getInteger("metadata");
+            this.name = nbt.getString("name");
+        }
+    }
+
+}
