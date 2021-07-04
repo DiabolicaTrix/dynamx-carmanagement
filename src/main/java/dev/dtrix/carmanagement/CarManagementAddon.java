5a2bf306a9ad346ca2a9e5e2cc51be7f588503e6
Alexy Mantha <mrredstone231@gmail.com>
2021-07-04 14:31:41 -0400

    Change from java.util.logging.Logger to org.apache.logging.log4j.Logger

--- src/main/java/dev/dtrix/carmanagement/CarManagementAddon.java
+++ src/main/java/dev/dtrix/carmanagement/CarManagementAddon.java
@@ -1,22 +1,28 @@
 package dev.dtrix.carmanagement;
 
 import fr.dynamx.api.contentpack.DynamXAddon;
+import fr.dynamx.api.network.sync.SynchronizedVariablesRegistry;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.util.logging.Logger;
-
-@DynamXAddon(modid = CarManagementAddon.MODID, name = "Car Management Addon")
+@DynamXAddon(modid = CarManagementAddon.MODID, name = "Car Management Addon", version = "1.0")
 public class CarManagementAddon {
 
     public static final String MODID = "carmanagement";
 
-    private static final Logger LOGGER = Logger.getLogger("Car Management");
+    private static final Logger LOGGER = LogManager.getLogger(MODID);
 
     @DynamXAddon.AddonEventSubscriber
     public static void init()
     {
         LOGGER.info("Initializing Car Management Addon.");
 
+        //SynchronizedVariablesRegistry.addSyncVar(new ResourceLocation(MODID, "key_uuid"), )
     }
 
+    public static Logger getLogger() {
+        return LOGGER;
+    }
 }
