838817993198dfba17d2e04a3167e9a5b76b5adf
Alexy Mantha <mrredstone231@gmail.com>
2021-07-04 14:36:30 -0400

    Remove Garage ItemBlock from Items class

--- src/main/java/dev/dtrix/carmanagement/debug/block/Blocks.java
+++ src/main/java/dev/dtrix/carmanagement/debug/block/Blocks.java
@@ -1,7 +1,10 @@
 package dev.dtrix.carmanagement.debug.block;
 
+import dev.dtrix.carmanagement.CarManagementAddon;
 import dev.dtrix.carmanagement.debug.block.BlockGarage;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -11,9 +14,16 @@ public class Blocks {
 
     public static final Block GARAGE = new BlockGarage();
 
+
     @SubscribeEvent
     public static void onBlockRegister(RegistryEvent.Register<Block> event) {
         event.getRegistry().registerAll(GARAGE);
     }
+
+    public static void onItemRegister(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(new ItemBlock(Blocks.GARAGE).setRegistryName(CarManagementAddon.MODID, "garage"));
+    }
+
+
 
 }
--- src/main/java/dev/dtrix/carmanagement/mod/item/Items.java
+++ src/main/java/dev/dtrix/carmanagement/mod/item/Items.java
@@ -1,10 +1,8 @@
 package dev.dtrix.carmanagement.mod.item;
 
 import dev.dtrix.carmanagement.CarManagementAddon;
-import dev.dtrix.carmanagement.mod.block.Blocks;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
@@ -19,16 +17,10 @@ public class Items {
     //ITEMS
     public static final Item KEY = new ItemKey();
 
-    //BLOCKS
-    public static final ItemBlock BLOCK_GARAGE = (ItemBlock) new ItemBlock(Blocks.GARAGE).setRegistryName(CarManagementAddon.MODID, "garage");
-
     @SubscribeEvent
     public static void onItemRegister(RegistryEvent.Register<Item> event) {
         //ITEMS
         event.getRegistry().registerAll(KEY);
-
-        //BLOCKS
-        event.getRegistry().registerAll(BLOCK_GARAGE);
     }
 
     @SideOnly(Side.CLIENT)
@@ -36,9 +28,6 @@ public class Items {
     public static void onModelRegister(ModelRegistryEvent event) {
         //ITEMS
         registerModel(KEY);
-
-        //BLOCKS
-        registerModel(BLOCK_GARAGE);
     }
 
     @SideOnly(Side.CLIENT)
